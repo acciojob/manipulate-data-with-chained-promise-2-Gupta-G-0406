36e9b93e9ac9d704778da89567d8c9223c20404f
Akash Gupta <124385823+Gupta-G-0406@users.noreply.github.com>
2026-01-19 18:43:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+window.onload = function () {
+  let arr = [1, 2, 3, 4];
+
+  function promise1(array) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(array.filter((elem) => elem % 2 === 0));
+      }, 1000); 
+    });
+  }
+
+  function promise2(array) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(array.map((elem) => elem * 2));
+      }, 2000); 
+    });
+  }
+
+  promise1(arr)
+    .then((filteredArr) => {
+      document.getElementById("output").innerText = filteredArr.join(",");
+      return promise2(filteredArr);
+    })
+    .then((multipliedArr) => {
+      document.getElementById("output").innerText = multipliedArr.join(",");
+    })
+    .catch((err) => {
+      document.getElementById("output").innerText = "error";
+    });
+};
